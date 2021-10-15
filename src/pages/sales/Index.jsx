f8d8943120e185442266575f07017c5a3d50c005
Mauricio Cendales <malexcdl@hotmail.com>
2021-10-15 13:10:56 -0500

    Comenzando lista de botones

--- src/pages/sales/Index.jsx
+++ src/pages/sales/Index.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 
 const Sale = () => {
-    return <div>Esta es la página Inicial del administrador de ventas</div>;
+    return (
+        < div className="flex flex-col w-full h-full" >
+            <div className="flex p-7 justify-center font-bold text-xl">ADMINISTRADOR DE VENTAS</div>
+            <div className="flex p-7 justify-center">Seleccione la tarea que desea realizar:</div>
+            <div className="flex w-full h-full justify-center">
+                <ul className="flex flex-col justify-center">
+                    <li>
+                        <button>Registrar Venta Nueva</button>
+                    </li>
+                    <li>
+                        <button>Ver Lista de Ventas</button>
+                    </li>
+                    <li>
+                        <button>Buscar Venta</button>
+                    </li>
+                    <li>
+                        <button>Modificar/Actualizar Venta</button>
+                    </li>
+                </ul>
+            </div>
+        </div>)
+        ;
 }
 
 export default Sale
