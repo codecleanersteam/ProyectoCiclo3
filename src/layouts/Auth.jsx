feb0394a528492a4dc2af84c08b068e631e8fe93
Mauricio Cendales <malexcdl@hotmail.com>
2021-10-15 00:36:21 -0500

    Font awesome instalado

--- src/layouts/Auth.jsx
+++ src/layouts/Auth.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Footer from "components/Footer";
-// import Element from "components/element";
 
 // import Logo from "components/ImagenLogo";
 
@@ -9,7 +8,6 @@ const Auth = ({ children }) => {
     <div className="flex flex-col h-screen bg-gray-100">
       <div className="flex flex-col items-center h-screen py-2 px-4">
         <div className="w-full pt-20">
-          {/* <Element/> */}
           {/* <Logo/> */}
           {children}
         </div>
